2fa0db8fef17eb2247e9fd3c074b0389ff77c16e
AugustoTI <augustocesarti2021@gmail.com>
2022-01-03 17:39:30 -0300

    mock das requisições

--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -1,13 +1,67 @@
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import Home from './';
+
+const handlers = [
+    rest.get('https://jsonplaceholder.typicode.com/posts', async (req, res, ctx) => {
+        return res(
+            ctx.json([
+                {
+                    userId: 1,
+                    id: 1,
+                    title: 'title 1',
+                    body: 'body 1',
+                },
+                {
+                    userId: 2,
+                    id: 2,
+                    title: 'title 2',
+                    body: 'body 2',
+                },
+                {
+                    userId: 3,
+                    id: 3,
+                    title: 'title 3',
+                    body: 'body 3',
+                },
+            ])
+        );
+    }),
+    rest.get('https://jsonplaceholder.typicode.com/photos', async (req, res, ctx) => {
+        return res(
+            ctx.json([
+                {
+                    url: 'img1.png',
+                },
+                {
+                    url: 'img2.png',
+                },
+                {
+                    url: 'img3.png',
+                },
+            ])
+        );
+    }),
+];
+
+const server = setupServer(...handlers);
+
 describe('<Home />', () => {
-    test('teste one', () => {
-        expect(1).toBe(1);
+    beforeAll(() => {
+        server.listen();
     });
 
-    test('teste two', () => {
-        expect(1).toBe(1);
+    beforeEach(() => server.resetHandlers());
+
+    afterAll(() => {
+        server.close();
     });
 
-    test('teste three', () => {
-        expect(1).toBe(1);
+    it('should render search, posts and load more', async () => {
+        render(<Home />);
+        const noMorePosts = screen.getByText('There are no more posts :(');
+
+        await waitForElementToBeRemoved(noMorePosts);
     });
 });
